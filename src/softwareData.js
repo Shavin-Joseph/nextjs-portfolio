2d8a5ac1030aea2766c4b6a7e69d9afe29fa4d0e
Shavin-Joseph <2023t01868@stu.cmb.ac.lk>
2025-11-16 21:00:47 +0530

    added software links

--- src/softwareData.js
+++ src/softwareData.js
@@ -8,7 +8,7 @@ export const software = [
     description: 'Your complete solution for tracking air conditioner installations and service history. A NEW VERTION RELEASED. SO PLEASE DOWNLOAD IT.',
     version: '1.0.0',
     releaseDate: '2025-10-07',
-    filePath: '', // Path to the file in the /public folder
+    filePath: 'https://drive.google.com/file/d/1RSiRuT0PU1-yvwjQGCw9AvvlMxYYAA_z/view?usp=sharing', // Path to the file in the /public folder
     passwordKey: 'COOL_APP_PASSWORD', // The key for the password in .env.local
   },
 
@@ -19,7 +19,7 @@ export const software = [
     description: 'A new page added named parts. The page is works with a mobile app and helpfull to track part removing process.',
     version: '1.0.1',
     releaseDate: '2025-11-16',
-    filePath: '/software/app.zip', // Path to the file in the /public folder
+    filePath: 'https://drive.google.com/file/d/1JEuKZzdTxYsFMBl2W8K1HkmxGy0Pdxxx/view?usp=sharing', // Path to the file in the /public folder
     passwordKey: 'COOL_APP_PASSWORD', // The key for the password in .env.local
   },
 
